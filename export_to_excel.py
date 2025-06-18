c243c19c7e1d7b3c55c74f64349aba73ded522ec
Simen Uthaug <sut020@uib.no>
2025-06-18 12:38:32 +0200

    Added code explanations.

--- export_to_excel.py
+++ export_to_excel.py
@@ -3,19 +3,24 @@ import time
 from get_companies_from_API import all_companies
 from RoleScraperAPI import get_roles_for_company  
 
+#Creates a list variable that will be used for storing data before turning it into a df and then into excel
 rows = []
 
+#goes through each item and links datavalues to a variable
 for company in all_companies:
     orgnr = company["orgnr"]
     seleskapsnavn = company["navn"]
     stiftet = company["stiftelsesdato"]
 
+    #using the function for getting roles and storing them in roles variable.
     roles = get_roles_for_company(orgnr)
 
+    #Checks roles and then iterates through each group data stored in roles.
     if roles:
         for gruppe in roles:
             gruppebeskrivelse = gruppe.get("type", {}).get("beskrivelse")
 
+            #name is split into first, middle and last name this fixes it and joins them into a single string. 
             for rolle in gruppe.get("roller", []):
                 person = rolle.get("person", {})
                 navn_dict = person.get("navn", {})
@@ -29,6 +34,7 @@ for company in all_companies:
                 fodselsdato = person.get("fodselsdato")
                 rollebeskrivelse = rolle.get("type", {}).get("beskrivelse")
 
+                #adds retrieved data into a dict that will be used to create a structured excel file.
                 rows.append({
                     "Selskapsnavn": seleskapsnavn,
                     "Organisasjonsnummer": orgnr,
@@ -41,6 +47,7 @@ for company in all_companies:
 
     time.sleep(0.5)
 
+#turns rows into a dataframe and uses the dataframe to write all retrieved data into an excel file. 
 df = pd.DataFrame(rows)
 df.to_excel("selskaper_og_roller.xlsx", index=False)
 print("fil lagret")
