eb49a1f53b068c7681f0b7427561dbb61b15dac4
Simen Uthaug <sut020@uib.no>
2025-06-18 12:32:54 +0200

    Added code explanation.

--- RoleScraperAPI.py
+++ RoleScraperAPI.py
@@ -3,23 +3,26 @@ import requests
 import time
 
 
-
+#used the all_companies list and returns orgnr for each item in the list
 def retrieve_orgnr():
     return [company['orgnr'] for company in all_companies]
-
+#saves the orgnrs in a new list
 org_lst = retrieve_orgnr()
 
+# function for getting the roles in a company based on orgnr. 
 def get_roles_for_company(orgnr):
     url = f"https://data.brreg.no/enhetsregisteret/api/enheter/{orgnr}/roller"
     response = requests.get(url)
 
+    #ensures that http response was successful before continuing 
     if response.status_code == 200:
         data = response.json()
         return data.get("rollegrupper", [])
     else:
         print(f"feil ved henting av roller for org: {orgnr}: {response.status_code}")
         return None
-    
+
+#uses above function to get the roles then the loop iterates through each companies roles and retrieves data like, name, role and description
 for orgnr in org_lst[:5]:
     print(f"\nRoller for {orgnr}:")
     roles = get_roles_for_company(orgnr)
